5b42599fbaad55a2931c0790c33d52354a3278cb
Ahnaffaiz <faiz.putra553@gmail.com>
2024-10-21 12:01:51 +0700

    fix: storage

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', App\Livewire\Home::class)->name('home');
 Route::get('/{slug}', App\Livewire\Detail::class)->name('detail');
 Route::get('/daftar/{slug}', App\Livewire\Daftar::class)->name('daftar');
-Route::get('/create-symlink', function (){
-    symlink(storage_path('/app/public'), public_path('storage'));
-    echo "Symlink Created. Thanks";
-});
